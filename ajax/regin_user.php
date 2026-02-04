36bb2717d6cb90d1c4f7d5ec898a5d4f0fc1200a
twozziey <twozziey@mail.ru>
2026-02-04 06:45:14 +0500

    Шаг 7.ZaharZuev

--- ajax/regin_user.php
+++ ajax/regin_user.php
@@ -1,6 +1,7 @@
 <?php
 	session_start();
 	include("../settings/connect_datebase.php");
+	require_once("../libs/autoload.php");
 	
 	$login = $_POST['login'];
 	$password = $_POST['password'];
@@ -12,13 +13,26 @@
 	if($user_read = $query_user->fetch_row()) {
 		echo $id;
 	} else {
-		$mysqli->query("INSERT INTO `users`(`login`, `password`, `roll`) VALUES ('".$login."', '".$password."', 0)");
-		
-		$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
-		$user_new = $query_user->fetch_row();
-		$id = $user_new[0];
-			
-		if($id != -1) $_SESSION['user'] = $id; // запоминаем пользователя
-		echo $id;
+		if(isset($_POST["g-recaptcha-response"]) == false) {
+			echo "Пройдите капчу";
+			exit;
+		}
+		$Secret = "";
+		$Recaptcha = new \ReCaptcha\ReCaptcha($Secret);
+
+		$Response = $Recaptcha->verify($_POST["g-recaptcha-response"], $_SERVER['REMOTE_ADDR']);
+
+		if($Response->isSuccess()) {
+			$mysqli->query("INSERT INTO `users`(`login`, `password`, `roll`) VALUES ('".$login."', '".$password."', 0)");
+			$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
+			$user_new = $query_user->fetch_row();
+			$id = $user_new[0];
+
+			if($id != -1) $_SESSION['user'] = $id; 
+			echo $id;
+		} else {
+			echo "Пользователь не распознан";
+			exit;
+		}
 	}
 ?>
